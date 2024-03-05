18f630c65622952e64d01ff91855aec1eb6f829a
Larilw <llarissawong@gmail.com>
2024-03-05 20:13:01 -0300

    feat: add Dockerfile and docker-compose to test project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Teste from "./pages/teste";
+import { SWRConfig } from "swr";
 
 export default function Home() {
   return <Teste />;
--- src/app/pages/teste.tsx
+++ src/app/pages/teste.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Box,
@@ -7,31 +8,30 @@ import {
   List,
   ListItem,
   ListItemText,
-  Fab,
   IconButton,
   Divider,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-
-const apiData = [
-  { id: 1, text: "Item 1" },
-  { id: 2, text: "Item 2" },
-  { id: 3, text: "Item 3" },
-  { id: 4, text: "Item 4" },
-  { id: 5, text: "Item 5" },
-  { id: 6, text: "Item 6" },
-  { id: 7, text: "Item 7" },
-  { id: 8, text: "Item 8" },
-  { id: 9, text: "Item 9" },
-  { id: 10, text: "Item 10" },
-  { id: 11, text: "Item 11" },
-  { id: 12, text: "Item 12" },
-  { id: 13, text: "Item 13" },
-  { id: 14, text: "Item 14" },
-  { id: 15, text: "Item 15" },
-];
+import useSWR from "swr";
 
 const Teste = () => {
+  const [fetchData, setFetchData] = React.useState(false);
+
+  const {
+    data: apiData,
+    error,
+    isValidating,
+  } = useSWR(fetchData ? `http://localhost:3333/teste/listar/` : null, (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  const handleClick = () => {
+    setFetchData(true);
+  };
+
+  if (error) return <div>Erro ao carregar os dados.</div>;
+  if (!apiData && isValidating) return <div>Carregando...</div>;
+
   return (
     <Box
       height="100vh"
@@ -62,16 +62,20 @@ const Teste = () => {
             overflow: "auto",
           }}
         >
-          {apiData.map((item, index) => (
-            <React.Fragment key={item.id}>
+          {apiData?.result.map((item: any, index: number) => (
+            <React.Fragment key={item.idTesteProfissional}>
               <ListItem>
-                <ListItemText primary={item.text} />
+                <ListItemText primary={item.nomeTesteProfissional} />
               </ListItem>
-              {index !== apiData.length - 1 && <Divider />}
+              {index !== apiData.result.length - 1 && <Divider />}
             </React.Fragment>
           ))}
         </List>
-        <IconButton aria-label="search" sx={{ marginTop: "2px" }}>
+        <IconButton
+          aria-label="search"
+          onClick={handleClick}
+          sx={{ marginTop: "2px" }}
+        >
           <SearchIcon sx={{ color: "blue" }} />
         </IconButton>
       </Box>
